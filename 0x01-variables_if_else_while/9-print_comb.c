a4e94b838bd9c645e677b9044208bc951283f2b5
Pkufa1 <petronellakamuriwo95@gmail.com>
2023-02-19 14:43:52 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all combination of single digits
+ * return: Always 0
+ */
+int mani(void)
+{
+	int n;
+
+	for (n = 8;  n < 58; n++)
+	{
+		putchar(',');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}

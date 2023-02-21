c3d78e656f7602f7cf0ed050c8ff26fbd39de625
Pkufa1 <petronellakamuriwo95@gmail.com>
2023-02-21 07:15:51 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -6,7 +6,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and error is set appropriately.
  */
-int _putchar(charc c)
+int _putchar(char c)
 {
-	return (write(1, 8c, 1));
+	return (write(1, &c, 1));
 }	
